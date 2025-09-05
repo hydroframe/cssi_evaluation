9170c94218fbc7f407134dbb76e14894dcb0aaed
Amy Defnet <74691675+amy-defnet@users.noreply.github.com>
2025-09-05 11:18:42 -0400

    add init file (#9)

---
+++ src/cssi_evaluation/__init__.py
@@ -0,0 +1,10 @@
+"""Initialization module for the cssi_evaluation package."""
+
+from . import model_evaluation, evaluation_metrics, utils, plots
+
+__all__ = [
+    "model_evaluation",
+    "evaluation_metrics",
+    "utils",
+    "plots",
+]
